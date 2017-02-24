f71086e0fa948280b4bbb0061587eb843e4e15a2
Ryan Daulton <ryan@redshepardsoftware.com>
2017-02-23 22:55:16 -0600

    create customer (.js) file

---
+++ Create Customer/index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var stripe = require('stripe')(
+  "your_stripe_key"
+);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//create a customer w/ card on file
+exports.createCustomer = app.get("/cc/:email/:desc/:tok", function createCustomer (req,res){
+    //var routes = app.get("/cc/:email/:desc/:tok");
+  var customer = stripe.customers.create({
+    email: req.params.email,
+    description: req.params.desc,
+    card: req.params.tok,
+  }, function(err, customer) {
+    if(err) {
+      return res.send(JSON.stringify(err));
+    }
+    res.send(customer);
+  });
+});
